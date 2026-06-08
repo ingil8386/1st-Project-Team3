22a3f8b6a33b53feccb139521849a185be23c7fa
ingil8386 <ingil8386@gmail.com>
2026-06-08 16:06:24 +0900

    html>jsp

---
+++ FarmStory/farmstory/src/main/java/controller/community/NoticeController.java
@@ -0,0 +1,25 @@
+package controller.community;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/community/notice.do")
+public class NoticeController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        req.getRequestDispatcher("/WEB-INF/views/community/notice.jsp").forward(req, resp);
+    }
+}
---
+++ FarmStory/farmstory/src/main/java/controller/market/CartController.java
@@ -0,0 +1,25 @@
+package controller.market;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/market/cart.do")
+public class CartController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        req.getRequestDispatcher("/WEB-INF/views/market/cart.jsp").forward(req, resp);
+    }
+}
